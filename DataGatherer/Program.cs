5a46625b1a826ba8d5885706a1b5991064467539
Kieran Edge <kesedge94@gmail.com>
2024-12-31 13:28:39 +0000

    Adding the data accessing class for returning HTML data

--- DataGatherer/Program.cs
+++ DataGatherer/Program.cs
@@ -1,10 +1,16 @@
-﻿Console.WriteLine("The purpose of this program is to gather data from a remote website and save it to a JSON object");
-string dummyUrl = UrlFormatter.UrlCreator(2022, 2023);
-Console.WriteLine(dummyUrl);
+﻿using System;
+using System.Net.Http;
+using HtmlAgilityPack;
+
+Console.WriteLine("The purpose of this program is to gather data from a remote website and save it to a JSON object");
+string dummyUrl = UrlFormatting.UrlCreator(2022, 2023);
+Console.WriteLine($"Accessing data from: {dummyUrl}");
+string htmlContent = DataAccessing.HtmlAccesser(dummyUrl).Result;
+Console.WriteLine(htmlContent);
 Console.ReadKey();
 
 
-public static class UrlFormatter
+public static class UrlFormatting
 {
     /// <summary>
     /// Static class for constructing the url for accessing the data
@@ -18,4 +24,30 @@ public static class UrlFormatter
         return urlModifier;
     }
 
+}
+
+public static class DataAccessing
+{
+    /// <summary>
+    /// Class for returning entire HTML elements from a webpage
+    /// </summary>
+    /// 
+    public async static Task<string> HtmlAccesser(string url)
+    {
+        try
+        {
+            // Create an HttpClient instance
+            using HttpClient client = new HttpClient();
+
+            // Send a GET request to the URL and get the response
+            string htmlContent = await client.GetStringAsync(url);
+
+            return htmlContent;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred: {ex.Message}");
+            return "HTML could not be accessed";
+        }
+    }
 }
