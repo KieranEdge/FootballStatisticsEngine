96aae9504064fc1648520d43eeac1af60e0aa2a8
Kieran Edge <kesedge94@gmail.com>
2024-12-31 13:28:55 +0000

    Adding the data accessing class for returning HTML data

--- FootballStatisticsEngine/Program.cs
+++ FootballStatisticsEngine/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Text.Json.Serialization;
+
 Console.WriteLine("Welcome to the football statistics engine application!");
+
+public class DataGathering
+{
+    
+    private string _baseUrl;
+}
